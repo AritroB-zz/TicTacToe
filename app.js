f8e29c0fb535af88cae6c4c785a622070198585d
Aritro Basu <abasuusa2010@gmail.com>
2018-07-30 17:48:23 -0500

    Created interactive gameboard

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("WELCOME TO TIC TAC TOE!");
+$(document).ready(function(){
+    var moves = 1;
+    var play = true;
+
+    $("#gboard tr td").click(function(){
+        //if it is player one's turn, fill cell with X
+        // determine if move is odd
+        console.log('clicked');
+        console.log("hi" + $(this).text());
+        //if the cell is empty
+        if($(this).text()=="")
+        {
+            //input X for first player
+            if((moves%2==1))
+            {
+                $(this).append('X');
+            }
+            //input O for second player
+            else
+            {
+                $(this).append('O');
+            }
+        }
+        moves++;
+
+    })
+})
